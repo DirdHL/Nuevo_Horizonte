25319f694608e36a3bc5bef726f476b76676b3d1
zzenisx00 <roggerdr1106@gmail.com>
2025-11-17 12:41:56 -0500

    academia-62

--- negocios/cancha-sintetica/carabayllo/canchas-carabayllo.js
+++ negocios/cancha-sintetica/carabayllo/canchas-carabayllo.js
@@ -1,1 +1,27 @@
 document.getElementById("logo-link").onclick = () => location.reload();
+
+const botones = document.querySelectorAll(".selector-btn");
+const imagenCancha = document.getElementById("cancha-image");
+
+botones.forEach(btn => {
+    btn.addEventListener("click", () => {
+        
+        const target = btn.dataset.target;
+
+        if (target === "pinos") {
+            imagenCancha.style.opacity = 0;
+            setTimeout(() => {
+                imagenCancha.src = "../../../img/canchas-carabayllo/pinos-img.png";
+                imagenCancha.style.opacity = 1;
+            }, 300);
+        }
+
+        if (target === "brisass") {
+            imagenCancha.style.opacity = 0;
+            setTimeout(() => {
+                imagenCancha.src = "../../../img/canchas-carabayllo/brisas-img.png";
+                imagenCancha.style.opacity = 1;
+            }, 300);
+        }
+    });
+});
