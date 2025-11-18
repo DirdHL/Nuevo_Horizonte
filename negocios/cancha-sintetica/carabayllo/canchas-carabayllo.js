fc54942ceeed5eb6b38ec1e9750a296f30b66f1a
zzenisx00 <roggerdr1106@gmail.com>
2025-11-18 10:26:08 -0500

    academia-71

--- negocios/cancha-sintetica/carabayllo/canchas-carabayllo.js
+++ negocios/cancha-sintetica/carabayllo/canchas-carabayllo.js
@@ -50,11 +50,12 @@ botones.forEach(btn => {
             sources[0].srcset = data["400"];
             sources[1].srcset = data["500"];
             sources[2].srcset = data["600"];
-            sources[3].srcset = data["800"];
-            sources[4].srcset = data["900"];
-            sources[5].srcset = data["1000"];
-            sources[6].srcset = data["1100"];
-            sources[7].srcset = data["1200"];
+            sources[3].srcset = data["700"];
+            sources[4].srcset = data["800"];
+            sources[5].srcset = data["900"];
+            sources[6].srcset = data["1000"];
+            sources[7].srcset = data["1100"];
+            sources[8].srcset = data["1200"];
         }, 300);
     });
 });
@@ -133,7 +134,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 if (window.innerWidth <= 768) {
-  setTimeout(() => applyFilter("natacion"), 100);
+  setTimeout(() => applyFilter("pinos"), 100);
 } else {
   setTimeout(() => applyFilter("todo"), 100);
 }
