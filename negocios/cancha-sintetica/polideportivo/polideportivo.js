aaaafa4901d8f35bd1a30bcec2828cff3f94dc53
NuevoHorizonte <inversionesnuevohorizontesrl@gmail.com>
2025-12-19 12:11:18 -0500

    git-100

--- negocios/cancha-sintetica/polideportivo/polideportivo.js
+++ negocios/cancha-sintetica/polideportivo/polideportivo.js
@@ -1,7 +1,51 @@
-const btnMenu = document.querySelector(".btn-izquierda");
-const barra = document.querySelector(".rectangulo-superior");
+    /* ─────────────────────────────
+    HEADER MENU
+    ───────────────────────────── */
+    const btnMenu = document.querySelector(".btn-izquierda");
+    const barra = document.querySelector(".rectangulo-superior");
 
-btnMenu.addEventListener("click", () => {
+    btnMenu.addEventListener("click", () => {
     barra.classList.toggle("expandido");
-});
+    });
 
+
+  const track = document.querySelector('.carousel-track');
+const items = Array.from(document.querySelectorAll('.img-box'));
+
+const spacing = 420;  
+const speed = 0.005;   
+let progress = 0;
+
+function animate() {
+  progress += speed;
+
+  items.forEach((item, i) => {
+
+    const x = ((i - progress) * spacing);
+
+    const index = x / spacing;
+
+    const y = Math.abs(index) ** 2 * 120;
+    const rotate = index * 10;
+
+    // ocultar lejos
+    if (Math.abs(index) > 4) {
+      item.style.opacity = 0;
+      return;
+    }
+
+    item.style.opacity = 1;
+    item.style.transform = `
+      translate(-50%, -50%)
+      translateX(${x}px)
+      translateY(${y}px)
+      rotate(${rotate}deg)
+    `;
+
+    item.style.zIndex = 100 - Math.abs(index);
+  });
+
+  requestAnimationFrame(animate);
+}
+
+animate();
