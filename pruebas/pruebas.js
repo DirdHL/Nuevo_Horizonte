c65ca8c30590680a7ed672411bfe7d9f42317d90
NuevoHorizonte <inversionesnuevohorizontesrl@gmail.com>
2026-05-23 17:44:21 -0500

    git-173

--- pruebas/pruebas.js
+++ pruebas/pruebas.js
@@ -82,3 +82,33 @@
         );
 
     });
+
+
+
+const fechaObjetivo = new Date("2026-07-27T15:00:00").getTime();
+
+function actualizarContador() {
+    const ahora = new Date().getTime();
+    const diferencia = fechaObjetivo - ahora;
+
+    if (diferencia <= 0) {
+        document.getElementById("tiempo").innerHTML = "00 : 00 : 00 : 00";
+        return;
+    }
+
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferencia / (1000 * 60 * 60)) % 24);
+    const minutos = Math.floor((diferencia / (1000 * 60)) % 60);
+    const segundos = Math.floor((diferencia / 1000) % 60);
+    const formato = (num) => num.toString().padStart(2, "0");
+
+document.getElementById("tiempo").innerHTML = `
+    <span>${dias}</span>
+    <span>${formato(horas)}</span>
+    <span>${formato(minutos)}</span>
+    <span>${formato(segundos)}</span>
+`;
+}
+
+setInterval(actualizarContador, 1000);
+actualizarContador();
--- rifas/rifas.js
+++ rifas/rifas.js
@@ -82,3 +82,42 @@
         );
 
     });
+
+
+
+const fechaObjetivo = new Date("2026-07-27T15:00:00").getTime();
+
+function actualizarContador() {
+    const ahora = new Date().getTime();
+    const diferencia = fechaObjetivo - ahora;
+
+   if (diferencia <= 0) {
+
+    document.getElementById("dias").textContent = "00";
+
+    document.getElementById("horas").textContent = "00";
+
+    document.getElementById("minutos").textContent = "00";
+
+    document.getElementById("segundos").textContent = "00";
+
+    return;
+}
+
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferencia / (1000 * 60 * 60)) % 24);
+    const minutos = Math.floor((diferencia / (1000 * 60)) % 60);
+    const segundos = Math.floor((diferencia / 1000) % 60);
+    const formato = (num) => num.toString().padStart(2, "0");
+
+document.getElementById("dias").textContent = dias;
+
+document.getElementById("horas").textContent = formato(horas);
+
+document.getElementById("minutos").textContent = formato(minutos);
+
+document.getElementById("segundos").textContent = formato(segundos);
+}
+
+setInterval(actualizarContador, 1000);
+actualizarContador();
